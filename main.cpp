3688950145a07a246d549f987fb2210064abb0f1
Sanskar-Dwivedi <sanskardwivedi835@gmail.com>
2025-07-19 10:36:21 +0530

    Added README

--- main.cpp
+++ main.cpp
@@ -1,51 +1,56 @@
 #include <iostream>
 
-double askValue(double x){
-    std::cout<<"Enter a double: ";
-    std::cin>>x;
+double askValue(double x)
+{
+    std::cout << "Enter a double: ";
+    std::cin >> x;
     return x;
 }
 
-char askOpp(char opp){
-    std::cout<<"Enter +, -, *, or /: ";
-    std::cin>>opp;
+char askOpp(char opp)
+{
+    std::cout << "Enter +, -, *, or /: ";
+    std::cin >> opp;
     return opp;
 }
 
-double multi(double x,double y){
-    return x*y;
+double multi(double x, double y)
+{
+    return x * y;
 }
 
-double add(double x,double y){
-    return x+y;
+double add(double x, double y)
+{
+    return x + y;
 }
 
-double minus(double x, double y){
-    return x-y;
+double minus(double x, double y)
+{
+    return x - y;
 }
 
-double div(double x, double y){
-    return x/y;
+double div(double x, double y)
+{
+    return x / y;
 }
 
-int main(){
+int main()
+{
     double x{askValue(x)};
     double y{askValue(y)};
-    
+
     char opp{askOpp(opp)};
 
-    if (opp=='+')
-        std::cout<<add(x,y);
-    else if (opp=='-')
-        std::cout<<minus(x,y);    
-    else if (opp=='/')
-        std::cout<<div(x,y);
-    else if (opp=='*')
-        std::cout<<multi(x,y);
+    if (opp == '+')
+        std::cout << add(x, y) << "\n";
+    else if (opp == '-')
+        std::cout << minus(x, y) << "\n";
+    else if (opp == '/')
+        std::cout << div(x, y) << "\n";
+    else if (opp == '*')
+        std::cout << multi(x, y) << "\n";
     else
         return 0;
-    
 
     return 0;
-
 }
